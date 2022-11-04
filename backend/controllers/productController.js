efa26a6c97b4ccd7ff7c4d9459cb39b03fc39da4
mubinkhan17102 <mobin17102@gmail.com>
2022-11-04 17:51:35 +0600

    Create product schema and route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,18 @@
-exports.getAllProducts = (req, res)=>{
+const Product = require('../models/productModel')
+
+//Create product
+exports.createProduct = async (req, res, next)=>{
+    const product = await Product.create(req.body);
+    res.status(201).json({
+        success: true,
+        product
+    })
+}
+
+exports.getAllProducts = async (req, res)=>{
+    const products = await Product.find()
     res.status(200).json({
-        message: 'Hello'
+        success: true,
+        products
     })
 }
---
+++ backend/models/productModel.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose')
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    rating: {
+        type: Number,
+        default: 0
+    },
+    images: [
+        {
+            public_id: {
+                type: String,
+                required: true
+            },
+            url:{
+                type: String,
+                required: true
+            }
+        }
+    ],
+    category: {
+        type: String,
+        required: true
+    },
+    stock:{
+        type: Number,
+        required: true,
+        default: 1
+    },
+    reviews: [
+        {
+            name: {
+                type: String,
+                required: true
+            },
+            rating: {
+                type: String,
+                required: true
+            },
+            comment: {
+                type: String,
+                required: true
+            }
+        }
+    ]
+} , { timestamps: true })
+
+module.exports = mongoose.model('Product', productSchema)
