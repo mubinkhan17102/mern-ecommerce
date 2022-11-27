9cb8b5a3683505caad76c1535aa1736378a5639d
mubinkhan17102 <mobin17102@gmail.com>
2022-11-27 20:11:05 +0600

    Add order related crud operation

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,8 +1,29 @@
-const { newOrder } = require('../controllers/orderController')
-const { isAuthinticated, authorizedUser } = require('../middleware/auth')
+const express = require("express");
+const {
+  newOrder,
+  getSingleOrder,
+  myOrders,
+  getAllOrders,
+  updateOrder,
+  deleteOrder,
+} = require("../controllers/orderController");
+const router = express.Router();
 
-const router = require('express').Router()
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
+router.route("/order/new").post(isAuthenticatedUser, newOrder);
 
-router.route('order/new')
-    .post(isAuthinticated, authorizedUser('user'), newOrder)
+router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
+
+router.route("/orders/me").get(isAuthenticatedUser, myOrders);
+
+router
+  .route("/admin/orders")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders);
+
+router
+  .route("/admin/order/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder);
+
+module.exports = router;
